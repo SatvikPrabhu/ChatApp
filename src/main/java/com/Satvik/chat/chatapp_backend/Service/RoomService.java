6ebffb646b2efdebfcafc99cbc4553ae9d9ca8ad
SatvikPrabhu <satvikp.567@gmail.com>
2025-08-15 00:52:29 +0530

    1st

--- src/main/java/com/Satvik/chat/chatapp_backend/Service/RoomService.java
+++ src/main/java/com/Satvik/chat/chatapp_backend/Service/RoomService.java
@@ -9,10 +9,16 @@ import java.util.List;
 
 public interface RoomService {
     boolean findroom(String roomId);
+
     ResponseEntity<?> SaveRoom(String roomId);
+
     List<Room> getRoom();
+
     Room getRoomById(long id);
+
     Room findByRoomId(String roomId);
+
     ResponseEntity<List<Message>> getMessages(String roomId, int page, int size);
+
     ResponseEntity<?> addMessageToRoom(String roomId, MessageRequest messageRequest);
 }
